10398505af1f30fbeab70ad6f534ce3ca9db7005
pavani2105 <125651883+pavani2105@users.noreply.github.com>
2025-04-05 22:25:31 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,38 @@
+# Basic String Manipulation  
+a = "Hello, World!"  
+print(a)  
+
+b = "Python Programming"  
+c = b[:5]  
+print(c)  
+
+# String Methods  
+d = "python is fun".upper()  
+print(d)  
+
+e = "Python is fun".replace("fun", "awesome")  
+print(e)  
+
+# String Formatting  
+f = "John"  
+g = 25  
+h = f"My name is {f}, and I am {g} years old."  
+print(h)  
+
+i = 49.99  
+j = f"{i:.2f}"  
+print(j)  
+
+# String Functions  
+k = "Hello, how are you?".count('o')  
+print(k)  
+
+l = "Hello, world! Python is amazing.".find("world")  
+print(l)  
+
+# Reverse and Check  
+m = "Python"[::-1]  
+print(m)  
+
+n = "madam" == "madam"[::-1]  
+print(n)  
